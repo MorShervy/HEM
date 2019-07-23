6eb736081e21f40d5493a7b1a4481f131530be6a
Almog Bakhshi <almogbakhshi@gmail.com>
2019-07-23 13:03:10 +0300

    Test again

--- react-native/App.js
+++ react-native/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>App3</Text>
+      <Text>App4</Text>
     </View>
   );
 }
