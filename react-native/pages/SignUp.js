d48e03ce024da26a1f5d22efbff59c84bb6717a2
Almog Bakhshi <almogbakhshi@gmail.com>
2019-08-05 11:29:53 +0300

    a

--- react-native/pages/SignUp.js
+++ react-native/pages/SignUp.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import Logo from "../components/Logo";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 const SignUp = props => (
   <View style={styles.container}>
@@ -22,7 +29,6 @@ export default SignUp;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    alignItems: "center"
   }
 });
