e07755ba17c6c20da96c0155c3ea38b9012613a4
Almog Bakhshi <almogbakhshi@gmail.com>
2019-08-12 14:00:26 +0300

    a

--- react-native/components/Month.js
+++ react-native/components/Month.js
@@ -1,77 +1,67 @@
 import React from "react";
 import {
-  Animated,
-  Dimensions,
-  ScrollView,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  FlatList,
+  Dimensions
 } from "react-native";
 
-const SCREEN_WIDTH = Dimensions.get("window").width;
+const { width, height } = Dimensions.get("window");
+
+const month = [
+  { key: 1, value: "Jan" },
+  { key: 2, value: "Feb" },
+  { key: 3, value: "Mar" },
+  { key: 4, value: "Apr" },
+  { key: 5, value: "May" },
+  { key: 6, value: "Jan" },
+  { key: 7, value: "Jul" },
+  { key: 8, value: "Aug" },
+  { key: 9, value: "Sep" },
+  { key: 10, value: "Oct" },
+  { key: 11, value: "Nov" },
+  { key: 12, value: "Dec" }
+];
 
 const Month = props => {
-  const transitionAnimation = index => {
-    return {
-      transform: [
-        { perspective: 800 },
-        {
-          scale: props.xOffset.interpolate({
-            inputRange: [
-              (index - 1) * SCREEN_WIDTH,
-              index * SCREEN_WIDTH,
-              (index + 1) * SCREEN_WIDTH
-            ],
-            outputRange: [0.25, 1, 0.25]
-          })
-        },
-        {
-          rotateX: props.xOffset.interpolate({
-            inputRange: [
-              (index - 1) * SCREEN_WIDTH,
-              index * SCREEN_WIDTH,
-              (index + 1) * SCREEN_WIDTH
-            ],
-            outputRange: ["300deg", "0deg", "300deg"]
-          })
-        },
-        {
-          rotateY: props.xOffset.interpolate({
-            inputRange: [
-              (index - 1) * SCREEN_WIDTH,
-              index * SCREEN_WIDTH,
-              (index + 1) * SCREEN_WIDTH
-            ],
-            outputRange: ["-300deg", "0deg", "300deg"]
-          })
-        }
-      ]
-    };
-  };
+  const _renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.touchableOpacity}>
+      <View style={styles.graphSize}>
+        <View style={styles.graphFilled} />
+      </View>
+      <Text style={styles.text}>{item.value}</Text>
+    </TouchableOpacity>
+  );
 
   return (
-    <View style={styles.scrollPage}>
-      <Animated.View style={[styles.screen, transitionAnimation(props.index)]}>
-        <Text style={styles.text}>{props.text}</Text>
-      </Animated.View>
-    </View>
+    <FlatList
+      style={styles.flatList}
+      data={month}
+      renderItem={_renderItem}
+      keyExtractor={item => item.key.toString()}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+    />
   );
 };
 export default Month;
 
 const styles = StyleSheet.create({
-  scrollPage: {
-    width: SCREEN_WIDTH
-  },
-  screen: {
+  flatList: { flex: 1, width: width },
+  touchableOpacity: {
     flex: 1,
-    justifyContent: "center",
+    width: width / 5,
     alignItems: "center",
-    borderRadius: 25,
-    backgroundColor: "white"
+    marginHorizontal: 15
   },
-  text: {
-    fontSize: 35,
-    fontWeight: "bold"
-  }
+  graphSize: {
+    flex: 0.85,
+    width: "100%",
+    borderWidth: 1,
+    justifyContent: "flex-end"
+  },
+  graphFilled: { flex: 0.2, backgroundColor: "blue" },
+  text: { flex: 0.15 }
 });
